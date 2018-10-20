ac251bd9f34b7965890f7e4022ba2f4ed3d3fc6c
Robbie Baker <robbiethebakerman@googlemail.com>
2018-10-20 16:08:12 +0100

    Sets up console.rb file

---
+++ console.rb
@@ -0,0 +1,52 @@
+require('pry-byebug')
+require_relative('models/customer')
+require_relative('models/film')
+require_relative('models/ticket')
+
+# Customer.delete_all
+# Film.delete_all
+# Ticket.delete_all
+
+customer_bob = Customer.new ({
+  'name' => 'Bob Baker'
+  'fund' => 100
+})
+
+customer_jill = Customer.new ({
+  'name' => 'Jill Jeffreys'
+  'fund' => 75
+})
+
+customer_bob.save
+customer_jill.save
+
+film_shining = Film.new ({
+  'title' => 'The Shining'
+  'price' => 5
+})
+
+film_shining.save
+
+ticket_1 = Ticket.new ({
+  'customer_id' => customer_bob.id
+  'film_id' => film_shining.id
+  })
+
+ticket_1.save
+
+# customer_bob.name = 'Bobby-Boy Billy-Bakes'
+# customer_bob.update
+#
+# film_shining.name = 'The Shinning (Simpsons parody)'
+# film_shining.update
+#
+# ticket_1.customer_id = customer_jill.id
+# ticket1.update
+
+# customer_bob.delete
+# film_shining.delete
+# ticket_1.delete
+
+
+binding.pry
+nil
